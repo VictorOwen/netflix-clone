1b0f35cd0e51d7808131183a00e926ff6e6fbd61
Kajubi Victor Kiwanuka <122186526+VictorOwen@users.noreply.github.com>
2025-06-05 19:35:14 +0300

    initial commit

---
+++ src/firebase.jsx
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBCn9x04zmlwwoWAjFIqEc-FC_NXUgbFfM",
+    authDomain: "netflix-clone-65ab7.firebaseapp.com",
+    projectId: "netflix-clone-65ab7",
+    storageBucket: "netflix-clone-65ab7.firebasestorage.app",
+    messagingSenderId: "495921085680",
+    appId: "1:495921085680:web:f95f62eb8876b50d0b5635"
+  };
