b80acffd30f1917bf9f2c7b69f932a4182e17318
Kajubi Victor Kiwanuka <122186526+VictorOwen@users.noreply.github.com>
2025-06-18 00:00:04 +0300

    change

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -3,15 +3,16 @@ import './LoginScreen.css'
 import { BrowserRouter, Routes, Route } from "react-router";
 function LoginScreen() {
   return (
-    <div>
-      <BrowserRouter>
-        {!user ? (
-          <LoginScreen/>
-        ) : (
-          <HomeScreen/>
-        )}
+    <div className="loginScreen">
+      <div className="loginScreen_background">
+        <img
+          className="loginScreen_logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+          alt=""
+        />
 
-      </BrowserRouter>
+      </div>
+    
       
     </div>
   )
