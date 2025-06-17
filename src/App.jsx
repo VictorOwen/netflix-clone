dc3670f925ea8e34efa5a94d43bde6baab1389d8
Kajubi Victor Kiwanuka <122186526+VictorOwen@users.noreply.github.com>
2025-06-18 01:50:56 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React from "react"
 import "./App.css";
 import HomeScreen from './screens/HomeScreen';
-import Login from './screens/LoginScreen';
+import LoginScreen from './screens/LoginScreen';
 import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import './LoginScreen.css'
+import netflixLogo from '../assets/netflixlogo.png';
 import { BrowserRouter, Routes, Route } from "react-router";
+
 function LoginScreen() {
   return (
     <div className="loginScreen">
       <div className="loginScreen_background">
         <img
           className="loginScreen_logo"
-          src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+          src={netflixLogo}
           alt=""
         />
-
+        <button className="loginScreen_button">
+          Sign In
+        </button>
+        <div className="loginScreen_gradient" />
+        <div className="loginScreen_body">
+          <>
+          <h1>Unlimited movies, TV shows and more.</h1>
+          </>
+        </div>
       </div>
     
       
