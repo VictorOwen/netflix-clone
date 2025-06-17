b090d75ad049c5d6b38817b9be16d327d9cec0a7
Kajubi Victor Kiwanuka <122186526+VictorOwen@users.noreply.github.com>
2025-06-17 23:51:16 +0300

    login screen changes

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './LoginScreen.css'
-
+import { BrowserRouter, Routes, Route } from "react-router";
 function LoginScreen() {
   return (
     <div>
+      <BrowserRouter>
+        {!user ? (
+          <LoginScreen/>
+        ) : (
+          <HomeScreen/>
+        )}
+
+      </BrowserRouter>
       
     </div>
   )
